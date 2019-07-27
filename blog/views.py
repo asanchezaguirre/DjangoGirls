9ddbb68f97be89549f49bfd351e66b4368efe718
AdrianaSan <s.aguirre.adriana@gmail.com>
2019-07-27 17:41:21 -0500

    Formulario

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.contrib.auth.models import User
 
 # Create your views here.
 def Inicio(request, iduser=None):
@@ -8,3 +9,18 @@ def Inicio(request, iduser=None):
     else: 
         p=Post.objects.filter(author__id = iduser)
     return render(request, 'blog/home.html', { "publicaciones":p})
+
+def Publicar(request):
+    if request.method=="GET":
+        u=User.objects.filter(is_active = True)
+        return render(request, 'blog/publicar.html', { "usuario":u })
+    elif request.method=="POST":
+        us=User.objects.get(id=request.POST.get('user'))
+        p=Post()
+        p.author=us
+        p.title=request.POST.get('title')
+        p.text=request.POST.get('text')
+        p.save()
+        u=User.objects.filter(is_active = True)
+        return render(request, 'blog/publicar.html', { "usuario":u, "publicacion":p })
+
--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Inicio),
     path('autor/<int:iduser>', views.Inicio),
+    path('publicar', views.Publicar),
 ]
