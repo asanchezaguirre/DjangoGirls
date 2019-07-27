23f80b8a71c5f09ea31c442ba74d1f16e46bc4d8
AdrianaSan <s.aguirre.adriana@gmail.com>
2019-07-27 16:42:19 -0500

    Templete

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def Inicio(request, iduser=None):
+    if iduser is None:
+        p=Post.objects.all()
+    else: 
+        p=Post.objects.filter(author__id = iduser)
+    return render(request, 'blog/home.html', { "publicaciones":p})
--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Inicio),
+    path('autor/<int:iduser>', views.Inicio),
 ]
